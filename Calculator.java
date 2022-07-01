efc86e045dce8e368a81f12a5e501207b98d163a
Sada9401 <gobadeoo7@gmail.com>
2022-07-01 20:47:47 +0530

    update

---
+++ Calculator.java
@@ -0,0 +1,15 @@
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		
+		int n1=10;
+		int n2=20;
+		// TODO Auto-generated method stub
+		System.out.println("Addition= " + (n1+n2));
+		System.out.println("Subtraction= " + (n1-n2));
+		System.out.println("Multiplication= " + (n1*n2));
+		System.out.println("Division" + (n1/n2));
+	}
+
+}
